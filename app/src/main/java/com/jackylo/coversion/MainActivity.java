7d614ad7a444020f1344c6a707c43c4212132c50
jackycklo <jackycklo2@gmail.com>
2017-05-09 11:05:09 +0800

    add resources

--- app/src/main/java/com/jackylo/coversion/MainActivity.java
+++ app/src/main/java/com/jackylo/coversion/MainActivity.java
@@ -1,14 +1,97 @@
 package com.jackylo.coversion;
+//TextViewCompat, AppCompatSpinner, AppCompatEditText
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends ActionBarActivity {
+
+    private Spinner unitTypeSpinner;
+
+    private EditText amountTextView;
+
+    TextView teaspoonTextView, tablespoonTextView, cupTextView, ounceTextView,
+            pintTextView, quartTextView, gallonTextView, poundTextView,
+            milliliterTextView, literTextView, milligramTextView, kilogramTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Fills the spinner with the unit options
+        addItemsToUnitTypeSpinner();
+
+        // Add listener to the Spinner
+        addListenerToUnitTypeSpinner();
+
+        // Get a reference to the edit text view to retrieve the amount of the unit type
+        amountTextView = (EditText) findViewById(R.id.amount_text_view);
+
+        initializeTextViews();
+
+    }
+
+    public void initializeTextViews() {
+
+        teaspoonTextView = (TextView) findViewById(R.id.tsp_text_view);
+        tablespoonTextView = (TextView) findViewById(R.id.tbs_text_view);
+        cupTextView = (TextView) findViewById(R.id.cup_text_view);
+        ounceTextView = (TextView) findViewById(R.id.oz_text_view);
+        pintTextView = (TextView) findViewById(R.id.pint_text_view);
+        quartTextView = (TextView) findViewById(R.id.quart_text_view);
+        gallonTextView = (TextView) findViewById(R.id.gallon_text_view);
+        poundTextView = (TextView) findViewById(R.id.pound_text_view);
+        milliliterTextView = (TextView) findViewById(R.id.ml_text_view);
+        literTextView = (TextView) findViewById(R.id.liter_text_view);
+        milligramTextView = (TextView) findViewById(R.id.mg_text_view);
+        kilogramTextView = (TextView) findViewById(R.id.kg_text_view);
+
+    }
+
+    public void addItemsToUnitTypeSpinner() {
+
+        // Get a reference to the spinner
+        unitTypeSpinner = (Spinner) findViewById(R.id.unit_type_spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> unitTypeSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.conversion_types, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        unitTypeSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        unitTypeSpinner.setAdapter(unitTypeSpinnerAdapter);
+
+    }
+
+    public void addListenerToUnitTypeSpinner() {
+        unitTypeSpinner = (Spinner) findViewById(R.id.unit_type_spinner);
+        unitTypeSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View arg1, int pos, long arg3) {
+                // Get the item selected in the Spinner
+                String itemSelectedInSpinner = parent.getItemAtPosition(pos).toString();
+
+                // Verify if I'm converting from teaspoon so that I use the right
+                // conversion algorithm
+                //checkIfConvertingFromTsp(itemSelectedInSpinner);
+
+            }
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO maybe add something here later
+            }
+        });
     }
 }
